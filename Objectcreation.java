c33bb0355330bb0fae4ad4f66dbe4b3b72aa30db
Harsha-982 <82398710+Harsha-982@users.noreply.github.com>
2021-04-24 18:41:13 +0530

    Abstract Constructor

---
+++ Objectcreation.java
@@ -0,0 +1,29 @@
+package Abstraction;
+
+public abstract class Objectcreation {
+
+	int i;
+	int j;
+	Objectcreation(int i,int j){
+		this.i=i;
+		this.j=j;
+		System.out.println(this.hashCode());
+	}
+	abstract void run();
+	
+}
+class Number extends Objectcreation{
+	Number(int i,int j){
+		super(i,j);
+		System.out.println(this.hashCode());     // hashcode is for knowing about object creation for every object hashcode is different
+		System.out.println(i+j);
+	}
+	public void run() {
+		System.out.println(i+j);
+	}
+	public static void main(String[] args) {
+		Number num = new Number(10,20);
+		num.run();
+	}
+}
+
